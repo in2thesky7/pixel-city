5924435f6b34174f7d16b3fa08547280dabb56fc
Jonah Travis <master@Jonahs-iMac.local>
2018-08-05 15:52:42 -0400

    Adding map engagement.

--- pixel-city/AppDelegate.swift
+++ pixel-city/AppDelegate.swift
@@ -1,3 +1,4 @@
+
 //
 //  AppDelegate.swift
 //  pixel-city
--- pixel-city/Controller/MapVC.swift
+++ pixel-city/Controller/MapVC.swift
@@ -7,19 +7,57 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class MapVC: UIViewController {
 
+    @IBOutlet weak var mapView: MKMapView!
+    
+    var locationManager = CLLocationManager()
+    let authorizationStatus = CLLocationManager.authorizationStatus()
+    let regionRadius: Double = 1000
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+       mapView.delegate = self
+        locationManager.delegate = self
+        configureLocationServices()
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        
+        self.mapView.showsUserLocation = true
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func centerMapWasPressed(_ sender: Any) {
+        if authorizationStatus == .authorizedAlways || authorizationStatus == .authorizedWhenInUse {
+            centerMapOnUserLocation()
+        }
     }
+    
 
+}
 
+extension MapVC: MKMapViewDelegate {
+    func centerMapOnUserLocation() {
+        guard let coordinate = locationManager.location?.coordinate else { return }
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(coordinate, regionRadius * 2.0, regionRadius * 2.0)
+        mapView.setRegion(coordinateRegion, animated: true)
+    }
+    
+}
+
+extension MapVC: CLLocationManagerDelegate {
+    func configureLocationServices() {
+        if authorizationStatus == .notDetermined {
+            locationManager.requestAlwaysAuthorization()
+        } else {
+            return
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        centerMapOnUserLocation()
+    }
+    
 }
 
